5d2f77019144ffb3a3af4253f8e2ad28a5f1aa5a
Kerick Howlett <88661181+KerickHowlett@users.noreply.github.com>
2024-08-31 16:27:51 -0400

    feat: create cli entrypoint

--- packages/app/app.go
+++ packages/app/app.go
@@ -1,1 +1,27 @@
 package app
+
+type App struct{}
+
+func NewApp() (app *App, err error) {
+	app = &App{}
+
+	return app, nil
+}
+
+func Run() {
+	app, err := NewApp()
+
+	if err != nil {
+		panic(err)
+	}
+
+	app.Run()
+}
+
+func (app *App) Run() error {
+	return nil
+}
+
+func (app *App) Close() error {
+	return nil
+}
---
+++ packages/app/entrypoint.go
@@ -0,0 +1,5 @@
+package app
+
+func Start() {
+	Run() // TODO: Add application pre-run setup code here.
+}
