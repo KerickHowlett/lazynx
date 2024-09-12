a4f3cb22c6e4309c6f5ee70120ba0f8e01d95870
Kerick Howlett <88661181+KerickHowlett@users.noreply.github.com>
2024-09-12 18:01:46 -0400

    feat(gui): initialize gocui struct

--- packages/gui/gui.go
+++ packages/gui/gui.go
@@ -1,6 +1,44 @@
 package gui
 
-func Gui(name string) string {
-	result := "Gui " + name
-	return result
+import (
+	"github.com/jesseduffield/gocui"
+)
+
+var OverlappingEdges = false
+
+type GUI struct {
+	g *gocui.Gui
+}
+
+func (gui *GUI) Run() error {
+	g, err := gui.initGoCUI()
+	if err != nil {
+		return err
+	}
+
+	gui.g = g
+
+	return nil
+}
+
+func (gui *GUI) initGoCUI() (g *gocui.Gui, err error) {
+	g, err = gocui.NewGui(gocui.NewGuiOpts{
+		OutputMode:      gocui.OutputTrue,
+		SupportOverlaps: OverlappingEdges,
+		PlayRecording:   false,
+		Headless:        false,
+		Width:           0,
+		Height:          0,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	gui.g = g
+	return g, nil
+}
+
+func NewGUI() (gui *GUI) {
+	return &GUI{}
 }
