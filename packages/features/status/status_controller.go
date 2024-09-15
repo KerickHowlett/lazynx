a80cbd8fd1c215e582e61c855599a4689378ae2d
Kerick Howlett <88661181+KerickHowlett@users.noreply.github.com>
2024-09-15 15:17:42 -0400

    feat(status): create new status controller struct

--- packages/features/status/status_controller.go
+++ packages/features/status/status_controller.go
@@ -1,3 +1,13 @@
 package status
 
+import (
+	"packages/shared/types"
+)
+
 type StatusController struct{}
+
+var _ types.IController = &StatusController{}
+
+func NewStatusController() *StatusController {
+	return &StatusController{}
+}
