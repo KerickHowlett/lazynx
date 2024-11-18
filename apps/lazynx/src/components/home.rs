7f43e139c99057f2bd2c4a7abea74b636038ff9b
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-11-18 00:37:39 -0500

    refactor(lazynx): clean up draw method

--- apps/lazynx/src/components/home.rs
+++ apps/lazynx/src/components/home.rs
@@ -1,20 +1,11 @@
+use std::rc::Rc;
+
 use color_eyre::Result;
 use ratatui::{prelude::*, widgets::*};
 use tokio::sync::mpsc::UnboundedSender;
 
 use super::Component;
 use crate::{action::Action, config::Config};
-
-const LAZYNX_TITLE: &str = r#"
- _                     _   _
-| |                   | \ | |
-| |     __ _ _____   _|  \| |_  __
-| |    / _` |_  / | | | . ` \ \/ /
-| |___| (_| |/ /| |_| | |\  |>  <
-\_____/\__,_/___|\__, \_| \_/_/\_\
-                  __/ |
-                 |___ /
-"#;
 
 #[derive(Default)]
 pub struct Home {
@@ -25,6 +16,49 @@ pub struct Home {
 impl Home {
     pub fn new() -> Self {
         Self::default()
+    }
+
+    fn create_block(&self) -> Block {
+        return Block::default()
+            .title(" Status ")
+            .borders(Borders::ALL)
+            .title_alignment(Alignment::Left)
+            .border_type(BorderType::Rounded)
+            .padding(Padding::horizontal(2));
+    }
+
+    fn create_layout(&self, area: Rect) -> Rc<[Rect]> {
+        return Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(Constraint::from_lengths([10, 5]))
+            .split(area);
+    }
+
+    fn get_copyright(&self) -> Paragraph {
+        let current_year = chrono::Datelike::year(&chrono::Local::now());
+        let copyright = Span::from(format!(
+            "Copyright {} {} Kerick Howlett",
+            String::from('\u{00A9}'),
+            current_year
+        ));
+        return Paragraph::new(copyright);
+    }
+
+    fn get_header(&self) -> Paragraph {
+        // NOTE: Don't change the whitespace or alignment for ASCII art text.
+        //       Any changes to them will be reflected in the app itself.
+        const LAZYNX_TITLE: &str = r#"
+ _                     _   _
+| |                   | \ | |
+| |     __ _ _____   _|  \| |_  __
+| |    / _` |_  / | | | . ` \ \/ /
+| |___| (_| |/ /| |_| | |\  |>  <
+\_____/\__,_/___|\__, \_| \_/_/\_\
+                    __/ |
+                   |___ /
+"#;
+
+        return Paragraph::new(LAZYNX_TITLE);
     }
 }
 
@@ -53,28 +87,14 @@ impl Component for Home {
     }
 
     fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
-        let block = Block::default()
-            .title_top("Status")
-            .borders(Borders::ALL)
-            .title_alignment(Alignment::Left)
-            .border_type(BorderType::Rounded)
-            .padding(Padding::horizontal(2));
-
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints(Constraint::from_lengths([10, 5]))
-            .split(block.inner(area));
+        let block = self.create_block();
+        let chunks = self.create_layout(block.inner(area));
         frame.render_widget(block, area);
 
-        let header = Paragraph::new(LAZYNX_TITLE);
+        let header = self.get_header();
         frame.render_widget(header, chunks[0]);
 
-        let current_year = chrono::Datelike::year(&chrono::Local::now());
-        let copyright = Span::from(format!(
-            "Copyright {} {} Kerick Howlett",
-            String::from('\u{00A9}'),
-            current_year
-        ));
+        let copyright = self.get_copyright();
         frame.render_widget(copyright, chunks[1]);
 
         Ok(())
