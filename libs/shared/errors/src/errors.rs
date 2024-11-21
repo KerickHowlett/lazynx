5d98dbe9ab66a6fdd30d59f91bc1231b26769d6f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-11-21 11:05:30 -0500

    fix(errors): correct local library import

--- libs/shared/errors/src/errors.rs
+++ libs/shared/errors/src/errors.rs
@@ -3,6 +3,8 @@ use std::env;
 use color_eyre::Result;
 use tracing::error;
 
+use shared_tui::tui::Tui;
+
 pub fn init() -> Result<()> {
     let (panic_hook, eyre_hook) = color_eyre::config::HookBuilder::default()
         .panic_section(format!(
@@ -15,7 +17,7 @@ pub fn init() -> Result<()> {
         .into_hooks();
     eyre_hook.install()?;
     std::panic::set_hook(Box::new(move |panic_info| {
-        if let Ok(mut t) = tui::Tui::new() {
+        if let Ok(mut t) = Tui::new() {
             if let Err(r) = t.exit() {
                 error!("Unable to exit Terminal: {:?}", r);
             }
