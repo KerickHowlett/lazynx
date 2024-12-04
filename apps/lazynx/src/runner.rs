7fde6790d2ec3e94bf14c8debbfaff3617c794bd
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-12-03 21:40:50 -0500

    test(lazynx): create macro to dry event handler tests wip
    I'm attempting to create a macro but am having difficulty getting it to compile. I'm posting as-is
    so others can look at it.
    
    BREAKING CHANGE: will not compile

--- apps/lazynx/src/runner.rs
+++ apps/lazynx/src/runner.rs
@@ -152,3 +152,133 @@ impl Runner {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Runner;
+    use crate::assert_event_handler;
+    use app_config::Config;
+    use color_eyre::eyre::Result;
+    use common::{Action, Component, Event};
+    use pretty_assertions::assert_eq;
+    use ratatui::layout::Rect;
+    use tokio::sync::mpsc::unbounded_channel;
+
+    fn setup() -> Result<Runner> {
+        return Runner::new(Config::default(), 0.0, 0.0, vec![]);
+    }
+
+    #[test]
+    fn test_runner_instantiation() {
+        let runner = setup();
+        assert_eq!(
+            runner.is_ok(),
+            true,
+            "Runner did not instantiate: {:?}",
+            runner.err()
+        );
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_init() -> Result<()> {
+        let mut runner = setup()?;
+        let (tx, mut rx) = unbounded_channel();
+        runner.handle_event(Event::Init, &tx).await?;
+        assert_eq!(Some(Action::Init), rx.recv().await);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_quit() -> Result<()> {
+        let mut runner = Runner::new(Config::default(), 0.0, 0.0, vec![])?;
+        let (tx, mut rx) = unbounded_channel();
+        runner.handle_event(Event::Quit, &tx).await?;
+        assert_eq!(Some(Action::Quit), rx.recv().await);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_render() -> Result<()> {
+        let mut runner = Runner::new(Config::default(), 0.0, 0.0, vec![])?;
+        let (tx, mut rx) = unbounded_channel();
+        runner.handle_event(Event::Render, &tx).await?;
+        assert_eq!(Some(Action::Render), rx.recv().await);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_tick() -> Result<()> {
+        let mut runner = Runner::new(Config::default(), 0.0, 0.0, vec![])?;
+        let (tx, mut rx) = unbounded_channel();
+        runner.handle_event(Event::Tick, &tx).await?;
+        assert_eq!(Some(Action::Tick), rx.recv().await);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_resize() -> Result<()> {
+        let mut runner = Runner::new(Config::default(), 0.0, 0.0, vec![])?;
+        let (tx, mut rx) = unbounded_channel();
+        runner.handle_event(Event::Resize(1, 2), &tx).await?;
+        assert_eq!(Some(Action::Resize { x: 1, y: 2 }), rx.recv().await);
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_render_2() -> Result<()> {
+        assert_event_handler!(Event::Resize(1, 2), Action::Resize { x: 1, y: 2 });
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_any_other_event() -> Result<()> {
+        #[derive(Default)]
+        struct MockComponent {}
+
+        impl Component<Config> for MockComponent {
+            fn handle_events(&mut self, event: Event) -> Option<Action> {
+                match event {
+                    Event::Closed => Some(Action::Quit),
+                    _ => None,
+                }
+            }
+
+            fn draw(&mut self, _f: &mut tui::Frame, _area: Rect) {}
+        }
+
+        let mock = Box::new(MockComponent::default());
+        let mut runner = Runner::new(Config::default(), 0.0, 0.0, vec![mock])?;
+        let (tx, mut rx) = unbounded_channel();
+        runner.handle_event(Event::Closed, &tx).await?;
+        assert_eq!(Some(Action::Quit), rx.recv().await);
+
+        Ok(())
+    }
+
+    #[macro_export]
+    macro_rules! assert_event_handler {
+        ($event:expr, $action:expr) => {{
+            #[derive(Default)]
+            struct MockComponent {}
+
+            impl Component<Config> for MockComponent {
+                fn handle_events(&mut self, event: Event) -> Option<Action> {
+                    match event {
+                        $event => Some($action),
+                        _ => None,
+                    }
+                }
+
+                fn draw(&mut self, _f: &mut tui::Frame, _area: Rect) {}
+            }
+
+            let mock = Box::new(MockComponent::default());
+            let mut runner = Runner::new(Config::default(), 0.0, 0.0, vec![mock])?;
+            let (tx, mut rx) = unbounded_channel();
+
+            runner.handle_event($event, &tx).await?;
+
+            assert_eq!(Some($action), rx.recv().await);
+        }};
+    }
+}
