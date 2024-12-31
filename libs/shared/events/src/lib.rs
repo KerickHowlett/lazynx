d4007c55e26b7dce522946cd6511d1811852fa9f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2024-12-31 01:07:47 -0500

    refactor(events): remove unneeded channel handlers

--- libs/shared/events/src/lib.rs
+++ libs/shared/events/src/lib.rs
@@ -1,7 +1,3 @@
 mod event;
-mod events;
 
 mod event_loop;
-
-#[path = "./streams/streams.mod.rs"]
-mod streams;
--- libs/shared/events/src/streams/stream_name.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
-pub enum StreamName {
-    Ticks,
-    KeyRefresh,
-    Render,
-    Crossterm,
-}
