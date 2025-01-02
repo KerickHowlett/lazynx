de51de08715c012d537b1485f801f75390f4a705
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-01-01 21:25:59 -0500

    refactor(tui): streamline functionality

--- libs/shared/tui/src/lib.rs
+++ libs/shared/tui/src/lib.rs
@@ -1,5 +1,8 @@
 mod tui;
 pub use tui::{Frame, Tui};
 
+mod tui_config;
+pub use tui_config::TuiConfig;
+
 pub mod errors;
 pub mod logger;
---
+++ libs/shared/tui/src/tui_config.rs
@@ -0,0 +1,63 @@
+use std::io::{stdout, Stdout};
+
+use color_eyre::eyre::Result;
+use crossterm::{
+    event::{DisableBracketedPaste, DisableMouseCapture, EnableBracketedPaste, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{prelude::CrosstermBackend, Terminal};
+
+#[derive(Default)]
+pub struct TuiConfig {
+    pub enable_mouse: bool,
+    pub enable_paste: bool,
+}
+
+#[derive(Default)]
+pub struct Tui {
+    config: TuiConfig,
+}
+
+pub type TuiBackend = Terminal<CrosstermBackend<Stdout>>;
+
+impl Tui {
+    pub fn new(config: TuiConfig) -> Tui {
+        Tui { config }
+    }
+
+    pub fn init(self) -> Result<TuiBackend> {
+        enable_raw_mode()?;
+        execute!(stdout(), EnterAlternateScreen)?;
+
+        if self.config.enable_mouse {
+            execute!(stdout(), EnableMouseCapture)?;
+        }
+        if self.config.enable_paste {
+            execute!(stdout(), EnableBracketedPaste)?;
+        }
+
+        let backend = CrosstermBackend::new(stdout());
+        let mut terminal = Terminal::new(backend)?;
+
+        terminal.clear()?;
+        terminal.hide_cursor()?;
+
+        Ok(terminal)
+    }
+
+    pub fn restore(config: TuiConfig) -> Result<()> {
+        if config.enable_paste {
+            execute!(stdout(), DisableBracketedPaste)?;
+        }
+
+        if config.enable_mouse {
+            execute!(stdout(), DisableMouseCapture)?;
+        }
+
+        execute!(stdout(), LeaveAlternateScreen)?;
+        disable_raw_mode()?;
+
+        Ok(())
+    }
+}
