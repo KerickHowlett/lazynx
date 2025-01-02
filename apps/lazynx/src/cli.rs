9ff29978c4b06bd0e666b35e50efb8ded6b58b7f
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-01-01 19:57:37 -0500

    refactor(lazynx): remove version info abstraction

--- apps/lazynx/src/cli.rs
+++ apps/lazynx/src/cli.rs
@@ -1,11 +1,9 @@
 use clap::Parser;
 
-use utils::print_version_info;
-
 use crate::consts::CURRENT_VERSION;
 
 #[derive(Parser, Debug)]
-#[command(version = print_version_info(CURRENT_VERSION.to_string()), about)]
+#[command(version = format!("version {}", CURRENT_VERSION.to_string()), about)]
 pub struct CLI {
     /// Tick rate, i.e. number of ticks per second
     #[arg(short, long, value_name = "FLOAT", default_value_t = 4.0)]
