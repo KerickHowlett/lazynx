efecbc8e5136c8d1fff64bd455f10fb78e8496a3
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-01-11 22:29:49 -0500

    refactor(common): remove unneeded action enum options

--- libs/shared/common/src/action.rs
+++ libs/shared/common/src/action.rs
@@ -1,20 +1,12 @@
 use serde::{Deserialize, Serialize};
 use strum::Display;
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Display, Serialize, Deserialize, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Display, Serialize, Deserialize, Hash)]
 pub enum Action {
-    #[default]
-    ClearScreen,
     Error(String),
     Help,
     Init,
     Quit,
     Render,
-    Resize {
-        x: u16,
-        y: u16,
-    },
-    Resume,
-    Suspend,
-    Tick,
+    Resize { x: u16, y: u16 },
 }
