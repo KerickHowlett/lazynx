7db8aa76c33e35c59b6f5c7737ed119357433616
Kerick Howlett <8661181+KerickHowlett@users.noreply.github.com>
2025-01-24 00:37:21 -0500

    test(workspace): cover workspace tab widget snapshot and init method

--- libs/workspace/src/workspace_tab_widget.rs
+++ libs/workspace/src/workspace_tab_widget.rs
@@ -37,3 +37,84 @@ impl WorkspaceTabWidget {
             .padding(Padding::left(3));
     }
 }
+
+#[cfg(test)]
+mod workspace_tab_widget_tests {
+    use super::WorkspaceTabWidget;
+
+    use insta::assert_snapshot;
+    use ratatui::{backend::TestBackend, Terminal};
+    use std::{env, fs, path::PathBuf};
+
+    use pretty_assertions::assert_eq;
+
+    struct TestBed {
+        original_dir: PathBuf,
+        temp_dir: PathBuf,
+        terminal: Terminal<TestBackend>,
+        widget: WorkspaceTabWidget,
+    }
+
+    impl Default for TestBed {
+        fn default() -> Self {
+            return TestBed {
+                original_dir: PathBuf::new(),
+                temp_dir: PathBuf::new(),
+                terminal: Terminal::new(TestBackend::new(30, 3)).unwrap(),
+                widget: WorkspaceTabWidget::default(),
+            };
+        }
+    }
+
+    impl TestBed {
+        fn setup(&mut self) {
+            self.original_dir = env::current_dir().unwrap();
+            self.temp_dir = env::current_dir().unwrap().join("test");
+
+            fs::create_dir(self.temp_dir.clone()).unwrap();
+            env::set_current_dir(self.temp_dir.clone()).unwrap();
+        }
+
+        fn restore(&mut self) {
+            env::set_current_dir(self.original_dir.clone()).unwrap();
+            fs::remove_dir_all(self.temp_dir.clone()).unwrap();
+        }
+    }
+
+    #[test]
+    fn test_init() {
+        let mut test_bed = TestBed::default();
+
+        test_bed.setup();
+        let expected = test_bed.temp_dir.file_name().unwrap().to_str().unwrap();
+
+        test_bed.widget.init().unwrap();
+
+        assert_ne!(
+            test_bed.widget.workspace_name, "",
+            "Workspace name was empty"
+        );
+        assert_eq!(
+            test_bed.widget.workspace_name, expected,
+            "Workspace name did not match"
+        );
+
+        test_bed.restore();
+    }
+
+    #[test]
+    fn test_draw_widget() {
+        let mut test_bed = TestBed::default();
+        test_bed.setup();
+
+        test_bed.widget.init().unwrap();
+        test_bed
+            .terminal
+            .draw(|f| test_bed.widget.draw(f, f.area()))
+            .unwrap();
+
+        assert_snapshot!(test_bed.terminal.backend());
+
+        test_bed.restore();
+    }
+}
